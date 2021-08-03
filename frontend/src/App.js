f7acc4ff5515d9abcd714c2a208a776e293da978
fitsumlegesse <fitsumlegesse@icloud.com>
2021-08-02 23:41:55 -0700

    creates backend dir  and moves data into it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import TestScreen from './screens/TestScreen'
 import ProductScreen from './screens/ProductScreen'
 
 
@@ -14,6 +15,7 @@ const App =  () => {
     <main className='py-3' className='body-color'>
       <Container > 
        <Route path='/' component={HomeScreen} exact />
+       <Route path='/test' component={TestScreen} exact />
        <Route path='/product/:id' component={ProductScreen}  />
       </Container>
     </main> 
