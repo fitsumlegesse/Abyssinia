2b5d225bf9bcc55fb957af87ba201166c807f2d7
fitsumlegesse <fitsumlegesse@icloud.com>
2021-08-17 00:50:56 -0700

    adds dontenv and converts es modules

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
-const { match } = require('assert')
-const express = require('express')
-const products = require('./data/products')
+// const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
+dotenv.config()
 
 const app = express()
 
@@ -16,4 +19,7 @@ app.get('/api/products/:id', (req, res)=>{
     const product = products.find((p) => p._id === req.params.id)
     res.json(product)
 })
-app.listen(5000, console.log('Server running on port 5000'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV}port ${PORT}`))
